513437dd54a1d009b364578d78f31a8414566203
shivanshuk <kaushal.shivanshu87@gmail.com>
2015-06-22 16:24:18 +0530

    Populate Menu Items

--- WebContent/js/menu.js
+++ WebContent/js/menu.js
@@ -12,9 +12,29 @@ $(function() {
 		$('#overlay').removeClass('blur-out');
 		$('#overlay').addClass('blur-in');
 		// $('.pop-up').hide();
-		$('.menu-pop-up').fadeIn(1000);
-		$('menu-pop-up').attr('display', 'block');
+		$('.menu-pop-up-main').fadeIn(1000);
+		$('menu-pop-up-main').attr('display', 'block');
 
 		return false;
 	});
+	
+	$('.menu-pop-up__close').click(function(e) {
+
+		$('menu-pop-up-main').fadeOut(700);
+		$('#overlay').removeClass('blur-in');
+		$('#overlay').addClass('blur-out');
+		$('menu-pop-up-main').attr('display', 'none');
+		e.stopPropagation();
+
+	});
+});
+
+$(function(){
+	$('#menu-cancel').click(function(e){
+		$('menu-pop-up-main').fadeOut(700);
+		$('#overlay').removeClass('blur-in');
+		$('#overlay').addClass('blur-out');
+		e.stopPropagation();
+	});
+	
 });
